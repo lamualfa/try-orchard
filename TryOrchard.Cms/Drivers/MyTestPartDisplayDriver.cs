f33834d052290aef9636375a03d10d3c6ba4ef36
Laode Muhammad Al Fatih <lamualfa@gmail.com>
2025-03-25 04:52:21 +0700

    feat: update orchard packages to v3

--- TryOrchard.Cms/Drivers/MyTestPartDisplayDriver.cs
+++ TryOrchard.Cms/Drivers/MyTestPartDisplayDriver.cs
@@ -12,46 +12,46 @@ namespace TryOrchard.Cms.Drivers
 {
     public sealed class MyTestPartDisplayDriver : ContentPartDisplayDriver<MyTestPart>
     {
-        private readonly IContentDefinitionManager _contentDefinitionManager;
+        // private readonly IContentDefinitionManager _contentDefinitionManager;
 
-        public MyTestPartDisplayDriver(IContentDefinitionManager contentDefinitionManager)
-        {
-            _contentDefinitionManager = contentDefinitionManager;
-        }
+        // public MyTestPartDisplayDriver(IContentDefinitionManager contentDefinitionManager)
+        // {
+        //     _contentDefinitionManager = contentDefinitionManager;
+        // }
 
-        public override IDisplayResult Display(MyTestPart part, BuildPartDisplayContext context)
-        {
-            return Initialize<MyTestPartViewModel>(GetDisplayShapeType(context), m => BuildViewModel(m, part, context))
-                .Location("Detail", "Content:10")
-                .Location("Summary", "Content:10");
-        }
+        // public override IDisplayResult Display(MyTestPart part, BuildPartDisplayContext context)
+        // {
+        //     return Initialize<MyTestPartViewModel>(GetDisplayShapeType(context), m => BuildViewModel(m, part, context))
+        //         .Location("Detail", "Content:10")
+        //         .Location("Summary", "Content:10");
+        // }
 
-        public override IDisplayResult Edit(MyTestPart part, BuildPartEditorContext context)
-        {
-            return Initialize<MyTestPartViewModel>(GetEditorShapeType(context), model =>
-            {
-                model.Show = part.Show;
-                model.ContentItem = part.ContentItem;
-                model.MyTestPart = part;
-            });
-        }
+        // public override IDisplayResult Edit(MyTestPart part, BuildPartEditorContext context)
+        // {
+        //     return Initialize<MyTestPartViewModel>(GetEditorShapeType(context), model =>
+        //     {
+        //         model.Show = part.Show;
+        //         model.ContentItem = part.ContentItem;
+        //         model.MyTestPart = part;
+        //     });
+        // }
 
-        public override async Task<IDisplayResult> UpdateAsync(MyTestPart model, IUpdateModel updater)
-        {
-            await updater.TryUpdateModelAsync(model, Prefix, t => t.Show);
+        // public override async Task<IDisplayResult> UpdateAsync(MyTestPart model, IUpdateModel updater)
+        // {
+        //     await updater.TryUpdateModelAsync(model, Prefix, t => t.Show);
 
-            return Edit(model);
-        }
+        //     return Edit(model);
+        // }
 
-        private static void BuildViewModel(MyTestPartViewModel model, MyTestPart part, BuildPartDisplayContext context)
-        {
-            var settings = context.TypePartDefinition.GetSettings<MyTestPartSettings>();
+        // private static void BuildViewModel(MyTestPartViewModel model, MyTestPart part, BuildPartDisplayContext context)
+        // {
+        //     var settings = context.TypePartDefinition.GetSettings<MyTestPartSettings>();
 
-            model.ContentItem = part.ContentItem;
-            model.MySetting = settings.MySetting;
-            model.Show = part.Show;
-            model.MyTestPart = part;
-            model.Settings = settings;
-        }
+        //     model.ContentItem = part.ContentItem;
+        //     model.MySetting = settings.MySetting;
+        //     model.Show = part.Show;
+        //     model.MyTestPart = part;
+        //     model.Settings = settings;
+        // }
     }
 }
